9f2ff5e23ea669b2a386d8dfa18ece94f3574f90
Akash-958 <kaushalakash08@gmail.com>
2023-02-20 13:18:51 +0530

    first commit

---
+++ src/main/java/com/Helloworld/Controller/controller.java
@@ -0,0 +1,17 @@
+package com.Helloworld.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class controller {
+
+    @GetMapping("hello")
+    public String hello() {
+        return "helloworld";
+    }
+
+
+}
